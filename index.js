ee3451dc4bb80a44f32750e7b0d6cbc1d4943af0
Sanjeev <sk3616593@gmail.com.com>
2023-07-16 11:54:05 +0530

    Add database and delete function to my contact list

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 const port = 8000;
@@ -7,13 +8,19 @@ const Contact = require('./models/contact');
 
 const app = express();
 
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
+
+// Set the views directory
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded());
+
+// Middleware to parse the request body
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the 'assets' directory
 app.use(express.static('assets'));
 
-
-
+// Temporary contact list (before integrating with database)
 var contactList = [
     {
         name: "Arpan",
@@ -29,69 +36,64 @@ var contactList = [
     }
 ]
 
+// Route for the practice page
 app.get('/practice', function (req, res) {
     return res.render('practice', {
         title: "Let us play with ejs"
     });
 });
 
-
+// Route for the home/contact list page
 app.get('/', function (req, res) {
-
-
-    Contact.find({name:"supu"} ,function(err,contacts){
+    // Fetch all contacts from the database
+    Contact.find({}, function(err, contacts){
         if(err){
-            console.log('error in fentching contactsfrom db');
-            return ;
+            console.log('Error in fetching contacts from the database');
+            return;
         }
-
-
+        // Render the home view with the fetched contacts
         return res.render('home', {
             title: "Contact List",
             contact_list: contacts
         });
-    });
-
-    
+    }); 
 });
+
+// Route to create a new contact
 app.post('/create-contact', function (req, res) {
-
-    /* contactList.push(req.body);
-    return res.redirect('/'); */
-
+    // Create a new contact in the database
     Contact.create({
         name: req.body.name,
         phone: req.body.phone
-
     }, function (err, newContact) {
         if (err) {
-            console.log('erroe in creating a contact!');
+            console.log('Error in creating a contact');
             return;
         }
-
-        console.log('*********', newContact);
+        console.log('New Contact:', newContact);
+        // Redirect back to the previous page
         return res.redirect('back');
     });
-
 });
 
+// Route to delete a contact
+app.get('/delete-contact', function (req, res) {
+    let id = req.query.id;
+    // Find and delete the contact from the database
+    Contact.findByIdAndDelete(id, function(err){
+        if(err){
+            console.log('Error in deleting an object from the database');
+            return;
+        }
+        // Redirect back to the previous page
+        return res.redirect('back');
+    });
+});
+
+// Start the server
 app.listen(port, function (err) {
     if (err) {
         console.log("Error in running the server", err);
     }
-    console.log('Yup!My Server is running on Port', port);
-})
-
-
-app.get('/delete-contact/', function (req, res) {
-    console.log(req.query);
-    let phone = req.query.phone
-
-    let contactindex = contactList.findIndex(contact => contact.phone == phone);
-
-    if (contactindex != -1) {
-        contactList.splice(contactindex, 1);
-    }
-
-    return res.redirect('back');
+    console.log('Server is running on Port', port);
 });
